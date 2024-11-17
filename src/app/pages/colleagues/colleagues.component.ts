2af079d15fb4215f5191ae627ce64adfb02d4c0c
polina <lolaschmidt@yandex.com>
2024-11-17 11:59:13 +0300

    colleagues

--- src/app/pages/colleagues/colleagues.component.ts
+++ src/app/pages/colleagues/colleagues.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 
 @Component({
   selector: 'colleagues',
   standalone: true,
-  imports: [],
+  imports: [NzCardModule, NzGridModule],
   templateUrl: './colleagues.component.html',
   styleUrl: './colleagues.component.css',
 })
